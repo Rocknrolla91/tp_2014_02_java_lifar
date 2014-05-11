b35240b707b1702c25beda840557bdc5fc2b2c45
Alena <rocknrolla@rocknrolla-X201EP.(none)>
2014-05-11 19:07:57 +0400

    Account session status added

---
+++ MavenTP/src/main/java/database/AccountSessionStatus.java
@@ -0,0 +1,12 @@
+package database;
+
+/**
+ * Created by rocknrolla on 5/11/14.
+ */
+public class AccountSessionStatus {
+    public static final String EXIST_USER = "User already exists";
+    public static final String INVALID_LOGIN= "User with this login is not found";
+    public static final String INVALID_PASS = "Wrong password";
+    public static final String EMPTY = "You must write here anything!";
+    public static final String DB_ERROR = "the database error";
+}
